75c5c30ed8035804ec59ef7c0687591c19d064ad
Lyuben <lsbworkedu@gmail.com>
2018-05-02 16:51:45 +0300

    added admin crud opps for books

--- src/BookSpace.Web/Areas/Admin/Controllers/AdminController.cs
+++ src/BookSpace.Web/Areas/Admin/Controllers/AdminController.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using BookSpace.Factories;
 using BookSpace.Models;
 using BookSpace.Repositories;
 using BookSpace.Repositories.Contracts;
 using BookSpace.Web.Areas.Admin.Models.ApplicationUserViewModels;
+using BookSpace.Web.Areas.Book.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -17,20 +19,24 @@ namespace BookSpace.Web.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-        private readonly IApplicationUserRepository repository;
+        private readonly IApplicationUserRepository userRepository;
+        private readonly IBookRepository bookRepository;
         private readonly IMapper objectMapper;
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly IBookFactory bookFactory;
 
-        public AdminController(IApplicationUserRepository repository, IMapper objectMapper, UserManager<ApplicationUser> userManager)
+        public AdminController(IApplicationUserRepository userRepository, IBookRepository bookRepository, IMapper objectMapper, UserManager<ApplicationUser> userManager, IBookFactory bookFactory)
         {
-            this.repository = repository;
+            this.userRepository = userRepository;
+            this.bookRepository = bookRepository;
             this.objectMapper = objectMapper;
             this.userManager = userManager;
+            this.bookFactory = bookFactory;
         }
 
         public IActionResult AllUsers()
         {
-            var users = this.repository.GetAllAsync().Result.ToList();
+            var users = this.userRepository.GetAllAsync().Result.ToList();
 
             var userViewModels = new List<ApplicationUserViewModel>();
 
@@ -47,10 +53,12 @@ namespace BookSpace.Web.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> EditUser(ApplicationUserViewModel userViewModel)
         {
-            var dbModel = this.repository.GetUserByUsernameAsync(userViewModel.Username).Result;
+            //TODO:Getting user by anything that can be changed is impossible!So I must use Id and therefore ID cannot be changed which is not good!
+            var dbModel = this.userRepository.GetByIdAsync(userViewModel.Id).Result;
 
             var user = this.objectMapper.Map(userViewModel, dbModel);
 
+            //TODO:THIS THROWS EX
             if (userViewModel.isAdmin)
             {
                 await this.userManager.AddToRoleAsync(user, "Admin");
@@ -60,34 +68,92 @@ namespace BookSpace.Web.Areas.Admin.Controllers
                 await this.userManager.RemoveFromRoleAsync(user, "Admin");
             }
 
-            //await this.repository.UpdateAsync(user);
-            await this.userManager.UpdateAsync(user);
+            await this.userRepository.UpdateAsync(user);
+            //await this.userManager.UpdateAsync(user);
 
             return this.RedirectToAction("AllUsers");
         }
-
-
-        public IActionResult EditUser(string username)
+       
+        public IActionResult EditUser(string id)
         {
-            var user = this.repository.GetUserByUsernameAsync(username).Result;
-            var userViewModel = objectMapper.Map<ApplicationUserViewModel>(user);
+            var dbModel = this.userRepository.GetByIdAsync(id).Result;
+            var userViewModel = objectMapper.Map<ApplicationUserViewModel>(dbModel);
 
             return View(userViewModel);
         }
-        //public IActionResult AllBooks()
-        //{
-        //    var users = this.repository.GetAllAsync().Result.ToList();
 
-        //    var mappedUsers = new List<ApplicationUserViewModel>();
+        public IActionResult AllBooks()
+        {
+            var allBooks = this.bookRepository.GetAllAsync().Result.ToList();
 
-        //    foreach (var user in users)
-        //    {
-        //        mappedUsers.Add(this.objectMapper.Map<ApplicationUserViewModel>(user));
-        //    }
+            var mappedBooks = new List<SimpleBookViewModel>();
+
+            foreach (var book in allBooks)
+            {
+                mappedBooks.Add(this.objectMapper.Map<SimpleBookViewModel>(book));
+            }
+
+            return View(mappedBooks);
+        }
+
+        [HttpPost("/EditBook/{bookid}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditBook(DetailedBookViewModel bookViewModel)
+        {
+            var dbModel = this.bookRepository.GetByIdAsync(bookViewModel.BookId).Result;
+
+            var book = this.objectMapper.Map(bookViewModel, dbModel);
+
+            await this.bookRepository.UpdateAsync(book);
+
+            return this.RedirectToAction("AllBooks");
+        }
+
+        [HttpGet("/EditBook/{bookid}")]
+        public IActionResult EditBook(string bookId) 
+        {
+           
+            var dbModel = this.bookRepository.GetByIdAsync(bookId).Result;
+
+            var bookViewModel = objectMapper.Map<DetailedBookViewModel>(dbModel);
+
+            return View(bookViewModel);
+        }
+
+        public async Task<IActionResult> DeleteBook(SimpleBookViewModel bookViewModel)
+        {
+            var dbModel = this.bookRepository.GetByIdAsync(bookViewModel.BookId).Result;
+
+            await this.bookRepository.RemoveBookAync(dbModel.BookId);
+
+            return this.RedirectToAction("AllBooks");
+        }
 
 
-        //    return View(mappedUsers);
-        //}
+        [HttpGet("/RegisterBook")]
+        public IActionResult RegisterBook()
+        {
+            return View();
+        }
+
+        [HttpPost("/RegisterBook")]
+        [ValidateAntiForgeryToken]
+        public IActionResult RegisterBook(SimpleBookViewModel bookViewModel)
+        {
+            var bookViewModelMap = this.objectMapper.Map<SimpleBookViewModel>(bookViewModel);
+            var bookToRegister = new BookSpace.Models.Book();
+
+            bookToRegister.BookId = new Guid().ToString();
+            bookToRegister.Isbn = bookViewModelMap.Isbn;
+            bookToRegister.Title = bookViewModelMap.Title;
+            bookToRegister.PublicationYear = bookViewModelMap.PublicationYear;
+            bookToRegister.CoverUrl = bookViewModelMap.CoverUrl;
+
+            //TODO:Does Not add book to data base
+            this.bookRepository.AddAsync(bookToRegister);
+
+            return RedirectToAction("Index");
+        }
 
         public IActionResult Index()
         {
