a4ffae0848364b299f33985a9a162c6863333840
Lyuben <lsbworkedu@gmail.com>
2018-05-03 15:05:33 +0300

    updated autmapper config added bookviewmodels to folder and fixed namespaces, fixed usermanager error

---
+++ src/BookSpace.Web/Models/BookViewModels/GenreViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BookSpace.Web.Models.BookViewModels
+{
+    public class GenreViewModel
+    {
+        public string GenreId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
