d76c44ef1c0452203444a7d10dc1e576a07e9361
stefani-nik <stefani_nik@abv.bg>
2018-05-03 18:59:25 +0300

    Configuring Blob Storage

---
+++ src/BookSpace.BlobStorage/BlobStorageService.cs
@@ -0,0 +1,69 @@
+﻿using BookSpace.BlobStorage.Contracts;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookSpace.BlobStorage
+{
+    public class BlobStorageService : IBlobStorageService
+    {
+        public readonly CloudBlobClient client;
+        public readonly SharedAccessBlobPolicy policy;
+
+        public BlobStorageService(BlobStorageInfo info)
+        {
+            var account = new CloudStorageAccount(new StorageCredentials(info.AccountName, info.Key), true);
+            this.client = account.CreateCloudBlobClient();
+            this.policy = new SharedAccessBlobPolicy()
+            {
+                Permissions = SharedAccessBlobPermissions.Read,
+                SharedAccessExpiryTime = DateTime.Now.AddDays(100)
+            };
+
+
+        }
+        public async Task<BlobObjectInfo> GetAsync(string name, string container)
+        {
+            var containerInstance = this.client.GetContainerReference(container);
+
+            if (!await containerInstance.ExistsAsync())
+            {
+                throw new ArgumentNullException();
+            }
+
+            var blob = await containerInstance.GetBlobReferenceFromServerAsync(name);
+            var sas = blob.GetSharedAccessSignature(policy);
+
+            return new BlobObjectInfo
+            {
+                Url = blob.Uri + sas
+            };
+
+        }
+
+        public async Task UploadAsync(string name, string container, Stream stream)
+        {
+            var containerInstance = this.client.GetContainerReference(container);
+
+            if (await containerInstance.CreateIfNotExistsAsync())
+            {
+               await containerInstance.SetPermissionsAsync(
+                    new BlobContainerPermissions()
+                    {
+                        PublicAccess = BlobContainerPublicAccessType.Off                 
+                    });
+
+            }
+
+            var blob = containerInstance.GetBlockBlobReference(name);
+
+            await blob.UploadFromStreamAsync(stream);
+
+        }
+    }
+}
---
+++ src/BookSpace.BlobStorage/Contracts/IBlobStorageService.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace BookSpace.BlobStorage.Contracts
+{
+    public interface IBlobStorageService
+    {
+        Task UploadAsync(string name, string container, Stream stream);
+        Task<BlobObjectInfo> GetAsync(string name, string container);
+    }
+}
