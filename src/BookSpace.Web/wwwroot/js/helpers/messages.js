c574a0496fd02e36df98c5a73e1f4e0d8a4cdb87
stefani-nik <stefani_nik@abv.bg>
2018-05-04 18:17:12 +0300

    Added succes failure and warning messages

---
+++ src/BookSpace.Web/wwwroot/js/helpers/messages.js
@@ -0,0 +1,14 @@
+﻿function showSuccessMessage(message) {
+    $("div.success").empty().append(`<p>${message}</p>`);
+    $("div.success").fadeIn(300).delay(1500).fadeOut(400);
+};
+
+function showFailureMessage(message) {
+    $("div.failure").empty().append(`<p>${message}</p>`);
+    $("div.failure").fadeIn(300).delay(1500).fadeOut(400);
+};
+
+function showWarningMessage(message) {
+    $("div.warning").empty().append(`<p>${message}</p>`);
+    $("div.warning").fadeIn(300).delay(1500).fadeOut(400);
+};
