18bfee5706a316876c9ae1d1de1b9d1f7b463d64
Yonko Borisov <yborisov92@gmail.com>
2018-05-05 21:01:05 +0300

    Fully implemented the search functionality.

--- src/BookSpace.Web/Controllers/SearchController.cs
+++ src/BookSpace.Web/Controllers/SearchController.cs
@@ -1,16 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
+using BookSpace.Models;
+using BookSpace.Repositories.Contracts;
+using BookSpace.Web.Models.BookViewModels;
+using BookSpace.Web.Models.GenreViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookSpace.Web.Controllers
 {
     public class SearchController : Controller
     {
-        public IActionResult SearchResult()
+        private readonly IBookRepository bookRepository;
+        private readonly IMapper objectMapper;
+        public SearchController(IBookRepository bookRepository, IMapper mapper)
         {
-            return View();
+            this.bookRepository = bookRepository;
+            this.objectMapper = mapper;
+        }
+        public async Task<IActionResult> SearchResult(string searchedString)
+        {
+            var searchedBooks = await bookRepository.Search(x => x.Title == searchedString || x.Author == searchedString);
+            var searchedBookViewModels = this.objectMapper.Map<IEnumerable<Book>, IEnumerable<SearchedBookViewModel>>(searchedBooks);
+
+            return View(searchedBookViewModels);
         }
     }
 }
