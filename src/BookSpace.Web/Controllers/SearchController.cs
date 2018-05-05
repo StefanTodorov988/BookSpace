501dfeb02d9c4ed855b53dc7590e3e78cf598df7
Yonko Borisov <yborisov92@gmail.com>
2018-05-05 15:34:31 +0300

    Working on implementing search functionality.

---
+++ src/BookSpace.Web/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSpace.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult SearchResult()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/BookSpace.Web/Models/BookViewModels/SearchedBookViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookSpace.Web.Models.BookViewModels
+{
+    public class SearchedBookViewModel
+    {
+        public string Title { get; set; }
+        public string ImgUrl { get; set; }
+    }
+}
