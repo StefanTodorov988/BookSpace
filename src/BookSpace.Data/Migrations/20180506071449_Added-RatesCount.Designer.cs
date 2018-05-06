334de4ef5877ce82f628f258452e0be71a921038
stefani-nik <stefani_nik@abv.bg>
2018-05-06 10:17:14 +0300

    Added RatesCount field to database

--- src/BookSpace.Data/Migrations/20180506071449_Added-RatesCount.Designer.cs
+++ src/BookSpace.Data/Migrations/20180506071449_Added-RatesCount.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace BookSpace.Data.Migrations
 {
     [DbContext(typeof(BookSpaceContext))]
-    partial class BookSpaceContextModelSnapshot : ModelSnapshot
+    [Migration("20180506071449_Added-RatesCount")]
+    partial class AddedRatesCount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/BookSpace.Data/Migrations/20180506071449_Added-RatesCount.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookSpace.Data.Migrations
+{
+    public partial class AddedRatesCount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RatesCount",
+                table: "Books",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RatesCount",
+                table: "Books");
+        }
+    }
+}
